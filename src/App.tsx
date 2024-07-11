ec90c0c9efaeccd4ade8103d8444659cbb03a658
Brad Teague <b_rad88@live.com>
2024-07-11 21:49:00 +0930

    Feat: Implement React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,35 @@
-import React, { useState } from "react";
-import { read } from "xlsx";
-import Workbook from "./Workbook";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Dashboard from "./features/dashboard/Dashboard";
+import LoadTemplatePage from "./features/template/LoadTemplatePage";
+import TemplatesPage from "./features/template/TemplatesPage";
+import EditTemplate from "./features/template/EditTemplatePage";
 
-import type { WorkBook } from "xlsx";
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Dashboard />,
+    children: [
+      { path: "" },
+      {
+        path: "templates",
+        element: <TemplatesPage />,
+        children: [
+          {
+            path: "",
+            element: <LoadTemplatePage />,
+          },
+          {
+            path: ":templateId",
+            element: <EditTemplate />,
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  const [wb, setWb] = useState<WorkBook | null>(null);
-  const onFileChange: React.ChangeEventHandler<HTMLInputElement> = async e => {
-    const file = e.target.files?.[0];
-    if (!file) return setWb(null);
-    const data = await file.arrayBuffer();
-    const wb = read(data, { dense: true });
-    setWb(wb);
-  };
-
-  return (
-    <div>
-      <div className="bg-zinc-900 p-4 print:hidden">
-        <label
-          htmlFor="input"
-          className="border-[2px] border-white rounded-lg py-3 px-6 hover:bg-white/10 cursor-pointer block w-fit text-white"
-        >
-          Upload Spreadsheet
-        </label>
-        <input
-          id="input"
-          type="file"
-          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-          onChange={onFileChange}
-          className="hidden"
-        />
-      </div>
-      {wb && <Workbook workbook={wb} />}
-    </div>
-  );
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
