b7c5e7a8fb5dd2e0e0041fe1db37f42bfca9a72f
Brad Teague <b_rad88@live.com>
2024-07-14 21:40:58 +0930

    UI: Home Icon link to dashboard

--- src/features/dashboard/Header.tsx
+++ src/features/dashboard/Header.tsx
@@ -1,10 +1,13 @@
+import { Link } from "react-router-dom";
 import { HomeIcon } from "../../common/icons";
 import Navbar from "./Navbar";
 
 function Header() {
   return (
     <header className="bg-zinc-800 h-full flex items-center px-8 gap-8 text-white">
-      <HomeIcon size={24} fill="white" />
+      <Link to={"/"}>
+        <HomeIcon size={24} fill="white" />
+      </Link>
       <Navbar />
     </header>
   );
