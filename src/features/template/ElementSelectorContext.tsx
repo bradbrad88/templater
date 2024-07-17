ff123e7a1d069dce0f95a1402c669becd513faac
Brad Teague <b_rad88@live.com>
2024-07-17 17:32:36 +0930

    Refactor: Simplify ElementSelectorContext
    Only hold element ID in state. Find element from elements array on each render

--- src/features/template/ElementSelectorContext.tsx
+++ src/features/template/ElementSelectorContext.tsx
@@ -1,10 +1,10 @@
-import React, { createContext, useEffect, useState } from "react";
+import React, { createContext, useState } from "react";
 import { TemplateElement } from "./template";
 import { useTemplate } from "./useTemplateContext";
 
 type ContextType = {
   selectedElement: TemplateElement | null;
-  selectElement: (element: TemplateElement) => void;
+  selectElement: (elementId: string) => void;
   deselect: () => void;
 };
 
@@ -18,27 +18,16 @@ export const ElementSelectorProvider = ({ children }: Props) => {
   const { template } = useTemplate();
   const elements = template?.elements;
   const [selectedElementId, setSeletedElementId] = useState<string | null>(null);
-  const [selectedElement, setSelectedElement] = useState<TemplateElement | null>(null);
-
-  useEffect(() => {
-    const findElement = (id: string): TemplateElement => {
-      const element = elements?.find(element => element.id === id);
-      if (!element) throw new Error("No element exists with id: " + id);
-      return element;
-    };
-    if (!selectedElementId && selectedElement) return setSelectedElement(null);
-    if (!selectedElementId) return;
-    const foundElement = findElement(selectedElementId);
-    if (selectedElement !== foundElement) return setSelectedElement(foundElement);
-  }, [selectedElementId, selectedElement, elements]);
 
   const deselect = () => {
     setSeletedElementId(null);
   };
 
-  const selectElement = (element: TemplateElement) => {
-    setSeletedElementId(element.id);
+  const selectElement = (elementId: string) => {
+    setSeletedElementId(elementId);
   };
+
+  const selectedElement = elements?.find(el => el.id === selectedElementId) || null;
 
   return (
     <Context.Provider value={{ selectedElement, selectElement, deselect }}>
