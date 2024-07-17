121961c34a9716b53e39242f94527ed0b7bae6e4
Brad Teague <b_rad88@live.com>
2024-07-17 17:25:26 +0930

    UI: Style button outline variant

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -12,7 +12,7 @@ const buttonVariants = cva(
         destructive:
           "bg-background text-destructive-foreground border-[1px] border-destructive shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
+          "border border-indigo-700 bg-white shadow-sm hover:bg-accent hover:text-accent-foreground hover:bg-indigo-700/10",
         secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
