0564bb4887f101215a7544b1bd3f912007da8c58
Brad Teague <b_rad88@live.com>
2024-07-26 12:33:21 +0930

    Feat: Refetch images on image upload
    Immediately display newly uploaded image with react-router query invalidation

--- src/features/images/imageQueries.ts
+++ src/features/images/imageQueries.ts
@@ -3,10 +3,10 @@ import { ImageFilters, getImageDetails, getImageList } from "./images";
 
 export const imageKeys = {
   all: () => ["images"] as const,
-  lists: () => [imageKeys.all(), "list"],
-  list: (filters: ImageFilters) => [imageKeys.lists(), filters] as const,
-  details: () => [imageKeys.all(), "details"] as const,
-  detail: (imageId: string) => [imageKeys.details(), imageId] as const,
+  lists: () => [...imageKeys.all(), "list"],
+  list: (filters: ImageFilters) => [...imageKeys.lists(), filters] as const,
+  details: () => [...imageKeys.all(), "details"] as const,
+  detail: (imageId: string) => [...imageKeys.details(), imageId] as const,
 };
 
 export const imageListQuery = () =>
--- src/features/images/useMutateImages.ts
+++ src/features/images/useMutateImages.ts
@@ -1,10 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Image, saveImage } from "./images";
+import { imageKeys } from "./imageQueries";
 
 export function useMutateImage() {
+  const client = useQueryClient();
   const mutation = useMutation({
     mutationFn: async (image: Image) => {
       return await saveImage(image);
+    },
+    onSettled: () => {
+      client.invalidateQueries({ queryKey: imageKeys.lists() });
     },
   });
   return { ...mutation };
